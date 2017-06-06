ca9214a818adff4f287472088d15a6dd17693d2c
evmon <evmon@ex.ua>
2017-06-06 15:51:04 +0300

    [15]: had 500, create new database and changed db name 'smart@smartrobotics' -> 'smart@smart'

--- smartrobot/settings.py
+++ smartrobot/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'smart$smartrobotics',
+        'NAME': 'smart$smart',
         'USER': 'smart',
         'PASSWORD': 'evmonrobot',
         'HOST': 'smart.mysql.pythonanywhere-services.com',
